560b60b5dec370fa7481ee8b9256d07cd43080ca
Valentyn Khalin <vkhalin@mirantis.com>
2020-02-21 10:59:37 +0200

    added custom logs def and custom time def

--- config.py
+++ config.py
@@ -1,19 +1,10 @@
 import logging
-import time
-import sys
 from datetime import datetime
 from pytz import timezone, utc
 import os
 
-#
-# LOGGER_CONFIG = {
-#     'level': logging.INFO,
-#     'file': 'logfile.log',
-#     'formatter': logging.Formatter('{asctime} {message} {funcName}', datefmt='%y.%m.%d %H:%M:%S', style='{')
-# }
 
-
-def customTime(*args):
+def custom_time(*args):
     utc_dt = utc.localize(datetime.utcnow())
     my_tz = timezone("Europe/Kiev")
     converted = utc_dt.astimezone(my_tz)
@@ -21,22 +12,27 @@ def customTime(*args):
 
 
 def log_msg(update):
-    return f'id: {update.effective_user.id} name: {update.effective_user.full_name} usrnm: {update.effective_user.username}'
+    return f'id: {update.effective_user.id} ' \
+           f'name: {update.effective_user.full_name} ' \
+           f'usrnm: {update.effective_user.username}'
 
 
 log = logging.getLogger('MainLogger')
-# set custom timezone for logging
-logging.Formatter.converter = customTime
+# TODO: set custom timezone for logging
+logging.Formatter.converter = custom_time
 fh = logging.FileHandler('logfile.log', encoding='utf-8')
 fh.setLevel(logging.INFO)
-fh.setFormatter(logging.Formatter('{asctime} {message} {funcName}', datefmt='%y.%m.%d %H:%M:%S', style='{'))
+fh.setFormatter(logging.Formatter('{asctime} {message} {funcName}',
+                                  datefmt='%y.%m.%d %H:%M:%S', style='{'))
 log.addHandler(fh)
 log.setLevel(logging.INFO)
 
 log_chat = logging.getLogger('ChatLogger')
-logging.Formatter.converter = customTime
-fh = logging.FileHandler(os.path.join('logfiles', 'log_chat.log'), encoding='utf-8')
+logging.Formatter.converter = custom_time
+fh = logging.FileHandler(os.path.join('logfiles', 'log_chat.log'),
+                         encoding='utf-8')
 fh.setLevel(logging.INFO)
-fh.setFormatter(logging.Formatter('{asctime} {message}', datefmt='%y.%m.%d %H:%M:%S', style='{'))
+fh.setFormatter(logging.Formatter('{asctime} {message}',
+                                  datefmt='%y.%m.%d %H:%M:%S', style='{'))
 log_chat.addHandler(fh)
 log_chat.setLevel(logging.INFO)
